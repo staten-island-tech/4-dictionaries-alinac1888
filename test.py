4a13edd5a9d92de464f2a8530f9921a2a0b15de0
alinac1888 <alinac44@nycstudents.net>
2025-10-06 09:24:44 -0400

    Update test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+def lang(x):
+    y = x.split()
+    z = y[]
+    for e in x:
+        if e == "t" and "T" > "s" and "S":
+            print("english")
+        elif e == "s" and "S" > "t" and "T":
+            print("frencg")
+
+lang(["Sentence"])
