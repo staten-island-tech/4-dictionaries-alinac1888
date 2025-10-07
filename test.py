9854a9705fd843c069abdd32e379d518b1b93c54
alinac1888 <alinac44@nycstudents.net>
2025-10-06 21:47:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 def lang(x):
-    y = x.split()
-    z = y[]
-    for e in x:
-        if e == "t" and "T" > "s" and "S":
-            print("english")
-        elif e == "s" and "S" > "t" and "T":
-            print("frencg")
+    t_count = []
+    s_count = []
 
-lang(["Sentence"])
+    for e in x:
+        if e == 't' or e == 'T':
+            t_count.append(e)
+        elif e == 's' or e == 'S':
+            s_count.append(e)
+
+        if len(t_count) > len(s_count):
+            print("English")
+        elif len(s_count) > len(t_count):
+            print("French")
+        elif len(s_count) == len(t_count):
+            print("Probably French")
+        break
+
+lang("The cat jumped from the trash pile into another trash pile")
